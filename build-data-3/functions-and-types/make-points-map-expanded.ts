21344216a1681a3c7cb94bad01697edf8b95809a
stu <stuvjordan@gmail.com>
2025-08-26 09:34:51 -0400

    makePointsMapExpanded fully drafted but not yet tested

--- build-data-3/functions-and-types/make-points-map-expanded.ts
+++ build-data-3/functions-and-types/make-points-map-expanded.ts
@@ -1,44 +1,6 @@
-import { all } from "arquero";
 import type { SegmentViews, Point, PointsMap } from "./types.ts";
 
-export function setUnsplitPoints(
-  pointsMap: PointsMap,
-  segmentViews: SegmentViews
-) {
-  const allPoints = segmentViews.unsplit.allPoints;
-  pointsMap.entries().forEach(([rg, rgVal]) => {
-    rgVal
-      .entries()
-      .filter(([wave, waveVal]) => waveVal !== null)
-      .forEach(([wave, waveVal]) => {
-        waveVal?.entries().forEach(([pg, pgVal]) => {
-          const spliceSize = pgVal.count;
-          if (typeof spliceSize !== "number") {
-            throw new Error(
-              `You passed a pointsMap to setUnsplitPoints that does not have count property set to a number at response group [${rg.toString()}], wave ${wave.toString(10)}, party group [${pg.toString()}]`
-            );
-          }
-          if (isNaN(spliceSize)) {
-            throw new Error(
-              `You passed a pointsMap to setUnsplitPoints that has a count of NaN at response group [${rg.toString()}], wave ${wave.toString(10)}, party group [${pg.toString()}]`
-            );
-          }
-          if (spliceSize < 0) {
-            throw new Error(
-              `You passed a pointsMap to setUnsplitPoints that has a negative count at response group [${rg.toString()}], wave ${wave.toString(10)}, party group [${pg.toString()}]`
-            );
-          }
-          const pointSlice = allPoints.splice(0, spliceSize);
-          if (pointSlice.length < spliceSize) {
-            throw new Error(
-              `The segmentViews object you passed to setUnsplitPoints map does not have enough points in it's unpslit segment to fill out the pointsMap you passed.`
-            );
-          }
-          pgVal.points.unsplit = pointSlice;
-        });
-      });
-  });
-}
+
 
 export function makePointsMapExpanded(segmentViews: SegmentViews) {
   const allPointsMapByResponse = segmentViews.expanded.byResponse;
@@ -50,16 +12,16 @@ export function makePointsMapExpanded(segmentViews: SegmentViews) {
         const allPointsByResponseAtRg =
           allPointsMapByResponse.get(rg)?.allPoints;
         //make a map that takes each party group to an array of all the points needed 
-        //at that party group for the byResponseAndParty view at rg,pg
+        //at that party group and the current rg for the byResponseAndParty view
         const segmentsByResponseAndPartyAtRg = segmentViews.expanded.byResponseAndParty.get(rg)
-        let allPointsMapByResponseAndPartyAtRg = undefined as undefined | Map<string[],Point[]>
+        let allPointsMapByResponseAndPartyAtRg = undefined as undefined | Map<string[], Point[]>
         if (segmentsByResponseAndPartyAtRg) {
           allPointsMapByResponseAndPartyAtRg = new Map(
-            segmentsByResponseAndPartyAtRg.entries().map(([pg,pgVal]) => ([
+            segmentsByResponseAndPartyAtRg.entries().map(([pg, pgVal]) => ([
               pg,
               pgVal.allPoints
-            ])) 
-          ) as Map<string[],Point[]>
+            ]))
+          ) as Map<string[], Point[]>
         }
         return [
           rg,
@@ -74,25 +36,20 @@ export function makePointsMapExpanded(segmentViews: SegmentViews) {
                 wave,
                 new Map(
                   waveVal.entries().map(([pg, pgVal]) => {
-                    let allPointsByResponseAndPartyAtRgAndPg = undefined;
-                    if (allPointsMapByResponseAndPartyAtRg) {
-                      allPointsByResponseAndPartyAtRgAndPg = allPointsMapByResponseAndPartyAtRg.get(pg)
-                    }
-                    if (allPointsByResponseAndPartyAtRgAndPg) {
-
-                    }
+                    const allPointsByResponseAndPartyAtRgAndPg = allPointsMapByResponseAndPartyAtRg ? allPointsMapByResponseAndPartyAtRg.get(pg) : [] as Point[]
+                    const nextByResponseAndPartySlice = allPointsByResponseAndPartyAtRgAndPg ? allPointsByResponseAndPartyAtRgAndPg.splice(0, pgVal.count) : [] as Point[]
                     return [
                       pg,
                       {
                         byResponse: allPointsByResponseAtRg
                           ? allPointsByResponseAtRg.splice(0, pgVal.count)
                           : ([] as Point[]),
-                        byResponseAndParty: allPointsMapByResponseAndPartyAtRg ? ,
+                        byResponseAndParty: nextByResponseAndPartySlice,
                         byResponseAndWave: allPointsByResponseAndWaveAtRgAndWave
                           ? allPointsByResponseAndWaveAtRgAndWave.splice(
-                              0,
-                              pgVal.count
-                            )
+                            0,
+                            pgVal.count
+                          )
                           : ([] as Point[]),
                         byResponseAndWaveAndParty: pgVal.allPoints,
                       },
