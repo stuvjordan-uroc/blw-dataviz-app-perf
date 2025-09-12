7c16e915d7186facd84717c97d4efeccbd9f05e9
stu <stuvjordan@gmail.com>
2025-09-12 17:34:04 -0400

    realized that the drawing data probably needs to be calculated inside the useEffect...and maybe made into a state variable?  Need to think about it.

--- src/hooks/useDrawingData.ts
+++ src/hooks/useDrawingData.ts
@@ -10,84 +10,86 @@ export default function useDrawingData(
   canvasesReady: boolean,
   canvasMap: React.RefObject<Map<string, HTMLCanvasElement> | null>
 ) {
+  const [drawingData, setDrawingData] = useState(null)
   const drawingData = coordinates.data === null || images.data === null || !canvasesReady
     ? null
-    : new Map(
-      coordinates.data.entries().map(([impVar, { pointGroups }]) => {
-        const noPartyImage = images.data.get("none");
-        const stage =
-          canvasMap.current && canvasMap.current.get(impVar)
-            ? new createjs.Stage(canvasMap.current.get(impVar)!)
-            : undefined;
-        const pointGroupsWithAssets = pointGroups.map((pointGroup) => {
-          const partyImage = images.data.get(pointGroup.pg.join("-"));
-          return {
-            ...pointGroup,
-            bitMapsNoParty: noPartyImage
-              ? pointGroup.coordinates.unsplit.map(
-                () => new createjs.Bitmap(noPartyImage)
-              )
-              : [],
-            bitMapsParty: partyImage
-              ? pointGroup.coordinates.unsplit.map(
-                () => new createjs.Bitmap(partyImage)
-              )
-              : [],
-          };
-        });
-        return [
-          impVar,
-          {
-            question: questions.prompts.find(
-              (v) => v.variable_name === impVar
-            )?.question_text,
-            stage: stage,
-            pointGroups: pointGroupsWithAssets,
-          },
-        ] as [
-            string,
-            {
-              question: string | undefined;
-              stage: createjs.Stage | undefined;
-              pointGroups: {
-                bitMapsNoParty: createjs.Bitmap[];
-                bitMapsParty: createjs.Bitmap[];
-                rg: string[];
-                wave: number;
-                pg: string[];
-                coordinates: PointsViews;
-              }[];
-            },
-          ];
-      })
-    );
-  //TODO define handlers for drawing
+    :
+    //TODO define handlers for drawing
 
-  //draw the initial view
-  useEffect(() => {
-    if (coordinates.data && images.data && canvasesReady && drawingData) {
-      drawingData.forEach(({ stage, pointGroups }) => {
-        if (stage) {
-          //clear the stage
-          stage.clear()
-          //loop through the pointGroups
-          pointGroups.forEach(({ bitMapsNoParty, coordinates }) => {
-            //add the no-party bitmaps for the current group to the stage
-            stage.addChild(...bitMapsNoParty)
-            //set the bitmaps x-s and y-s to the unsplit view coordinates
-            bitMapsNoParty.forEach((bm, bmIdx) => {
-              bm.set({
-                x: coordinates.unsplit[bmIdx].x,
-                y: coordinates.unsplit[bmIdx].y
+    //draw the initial view
+    useEffect(() => {
+      if (coordinates.data && images.data && canvasesReady) {
+        const newDrawingData = new Map(
+          coordinates.data.entries().map(([impVar, { pointGroups }]) => {
+            const noPartyImage = images.data.get("none");
+            const stage =
+              canvasMap.current && canvasMap.current.get(impVar)
+                ? new createjs.Stage(canvasMap.current.get(impVar)!)
+                : undefined;
+            const pointGroupsWithAssets = pointGroups.map((pointGroup) => {
+              const partyImage = images.data.get(pointGroup.pg.join("-"));
+              return {
+                ...pointGroup,
+                bitMapsNoParty: noPartyImage
+                  ? pointGroup.coordinates.unsplit.map(
+                    () => new createjs.Bitmap(noPartyImage)
+                  )
+                  : [],
+                bitMapsParty: partyImage
+                  ? pointGroup.coordinates.unsplit.map(
+                    () => new createjs.Bitmap(partyImage)
+                  )
+                  : [],
+              };
+            });
+            return [
+              impVar,
+              {
+                question: questions.prompts.find(
+                  (v) => v.variable_name === impVar
+                )?.question_text,
+                stage: stage,
+                pointGroups: pointGroupsWithAssets,
+              },
+            ] as [
+                string,
+                {
+                  question: string | undefined;
+                  stage: createjs.Stage | undefined;
+                  pointGroups: {
+                    bitMapsNoParty: createjs.Bitmap[];
+                    bitMapsParty: createjs.Bitmap[];
+                    rg: string[];
+                    wave: number;
+                    pg: string[];
+                    coordinates: PointsViews;
+                  }[];
+                },
+              ];
+          })
+        );
+        drawingData.forEach(({ stage, pointGroups }) => {
+          if (stage) {
+            //clear the stage
+            stage.clear()
+            //loop through the pointGroups
+            pointGroups.forEach(({ bitMapsNoParty, coordinates }) => {
+              //add the no-party bitmaps for the current group to the stage
+              stage.addChild(...bitMapsNoParty)
+              //set the bitmaps x-s and y-s to the unsplit view coordinates
+              bitMapsNoParty.forEach((bm, bmIdx) => {
+                bm.set({
+                  x: coordinates.unsplit[bmIdx].x,
+                  y: coordinates.unsplit[bmIdx].y
+                })
               })
             })
-          })
-          //update the stage
-          stage.update()
-        }
-      })
-    }
-  }, [coordinates, images, canvasesReady])
+            //update the stage
+            stage.update()
+          }
+        })
+      }
+    }, [coordinates, images, canvasesReady])
   //return the drawing data
   return drawingData
 }
