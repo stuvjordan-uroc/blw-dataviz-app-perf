046a7b91b98e7ef224fd3d07c2297f58ca788dd9
stu <stuvjordan@gmail.com>
2025-09-15 16:27:38 -0400

    roughing out controls.  typescript is fucking with me on defining the onchange event handler

--- src/components/viz/Controls.tsx
+++ src/components/viz/Controls.tsx
@@ -1,64 +1,69 @@
-import "./Controls.css";
-import { Root, Thumb } from "@radix-ui/react-switch";
-import type { RequestedView } from "../../hooks/use-view";
 import { useState } from "react";
+import type { FormEventHandler } from "react";
+import type { RequestedView } from "../../hooks/use-view";
+import "./Controls.css";
 export default function Controls({
   requestedView,
 }: {
   requestedView: React.RefObject<RequestedView>;
 }) {
-  const [rv, setRv] = useState<RequestedView>(requestedView.current);
+  const [rv, setRv] = useState(requestedView.current);
+  const rvData = rv.map((split, idx) => ({
+    splitId: idx === 0 ? "by-response" : idx === 1 ? "by-wave" : "by-party",
+    splitOnValue: idx === 0 ? "expanded" : idx === 1 ? "wave" : "party",
+    labelText:
+      idx === 0
+        ? "Split by response"
+        : idx === 1
+          ? "Split by wave"
+          : "Split by party",
+    checked: !(split === null),
+    split: split,
+    idx: idx,
+  }));
+  const handleBubbledChange = (e: FormEventHandler<HTMLFormElement>) => {
+    const inputChecked = (e.target as HTMLInputElement).checked;
+    const whichSplit = rvData.find(
+      ({ splitId }) => splitId === (e.target as HTMLInputElement).id
+    );
+    if (whichSplit) {
+      setRv((prevRv) => {
+        if (whichSplit.idx === 0 && inputChecked === false) {
+          return [null, null, null];
+        }
+        const newRv = [];
+        newRv.push(whichSplit.idx === 0 ? "expanded" : prevRv[0]);
+        newRv.push(
+          whichSplit.idx === 1
+            ? whichSplit.checked
+              ? rvData[1].splitOnValue
+              : null
+            : prevRv[1]
+        );
+        newRv.push(
+          whichSplit.idx === 2
+            ? whichSplit.checked
+              ? rvData[2].splitOnValue
+              : null
+            : prevRv[2]
+        );
+        return newRv as RequestedView;
+      });
+    }
+  };
   return (
-    <form
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "end",
-        gap: "0.5rem",
-      }}
-    >
-      <div className="split-by-option">
-        <label
-          className="Label"
-          htmlFor="by-response"
-          style={{ paddingRight: 15 }}
-        >
-          Split By Response
+    <form onChange={handleBubbledChange}>
+      {rvData.map(({ splitId, labelText, checked }) => (
+        <label key={splitId}>
+          {labelText}
+          <input
+            type="checkbox"
+            checked={checked}
+            id={splitId}
+            disabled={splitId === "by-response" ? false : rv[0] === null}
+          />
         </label>
-        <Root className="SwitchRoot" id="by-response" checked={rv[0] !== null}>
-          <Thumb className="SwitchThumb" />
-        </Root>
-      </div>
-      <div className="split-by-option">
-        <label className="Label" htmlFor="by-wave" style={{ paddingRight: 15 }}>
-          Split By Wave
-        </label>
-        <Root
-          className="SwitchRoot"
-          id="by-wave"
-          checked={rv[1] !== null}
-          disabled={rv[0] === null}
-        >
-          <Thumb className="SwitchThumb" />
-        </Root>
-      </div>
-      <div className="split-by-option">
-        <label
-          className="Label"
-          htmlFor="by-party"
-          style={{ paddingRight: 15 }}
-        >
-          Split By Party
-        </label>
-        <Root
-          className="SwitchRoot"
-          id="by-party"
-          checked={rv[2] !== null}
-          disabled={rv[0] === null}
-        >
-          <Thumb className="SwitchThumb" />
-        </Root>
-      </div>
+      ))}
     </form>
   );
 }
